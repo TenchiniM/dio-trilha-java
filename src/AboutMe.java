6c2155896bef1d8ec37b6bda734b162ff9b89555
tenchiniM <maycontss@gmail.com>
2023-01-04 19:06:47 -0300

    atualizações

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,15 +1,18 @@
 public class AboutMe {
     public static void main(String[] args) {
+        if ((10>15) < 20){
+            System.out.println("Verdade");
+        }
 
         
-        String nome = args [0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        Double altura = Double.valueOf(args[3]);;
-
-        System.out.println("Ola me chamo "+ nome+ " "+ sobrenome);
-        System.out.println("Tenho " + idade+ " anos ");
-        System.out.println("Minha altura é "+ altura + "cm");
+//        String nome = args [0];
+//        String sobrenome = args[1];
+//        int idade = Integer.valueOf(args[2]);
+//        Double altura = Double.valueOf(args[3]);;
+//
+//        System.out.println("Ola me chamo "+ nome+ " "+ sobrenome);
+//        System.out.println("Tenho " + idade+ " anos ");
+//        System.out.println("Minha altura é "+ altura + "cm");
     }
 
 }
